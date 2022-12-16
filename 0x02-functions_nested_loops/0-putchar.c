899a1d67227b7333f274b395fbfa754754f94858
codeained <jothamokoth@gmail.com>
2022-12-15 18:18:42 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		return(*sh);
+		sh++;
+	}
+	return('\n');
+
+	return (0);
+
+}
