ba60e911b0fe3d850970da7d121d5311a4083d83
codeained <jothamokoth@gmail.com>
2023-03-20 14:01:34 +0000

    C-hello world edits

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+/**
+ *main - C program that prints a given line to the standard error
+ *Return: 1 (SUccess)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar,
+	2015-10-19\n, stdout");
+	return (1);
+}
