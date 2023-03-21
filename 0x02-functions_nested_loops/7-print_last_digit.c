1f1c309c4a38265d6e05dfd7323e4e1068facfd9
codeained <jothamokoth@gmail.com>
2023-03-21 19:23:00 +0000

    cganges to 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,15 +11,13 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n;
-
-	if (n < 0)
-	{
-		n = -n;
-	}
+	int last_digit;
 
 	last_digit = n % 10;
+	if  (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
 	_putchar(last_digit + '0');
-
 	return (last_digit);
 }
