7916b5f78dde245b8431717a3dc7992f7717400c
codeained <jothamokoth@gmail.com>
2023-04-05 14:45:20 +0000

    change 4 to task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,12 +20,15 @@ int main(void)
 		} else if (a % 5 == 0 && a % 3 != 0)
 		{
 			printf("Buzz ");
-		} else if (a % 5 == 0 && a % 3 == 0)
+		} else if (a % 3 == 0 && a % 5 == 0)
 		{
 			printf("FizzBuzz ");
-		} else
+		} else if (a == 1)
 		{
 			printf("%d ", a);
+		} else
+		{
+			printf(" %d ", a);
 		}
 	}
 	printf("\n");
