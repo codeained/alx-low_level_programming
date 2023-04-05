883c9c6a8c9f6d831c63492063bda095d2e749cd
codeained <jothamokoth@gmail.com>
2023-04-05 08:24:30 +0000

    changes to task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,17 +6,25 @@
  */
 void print_diagonal(int n)
 {
-		int a, b;
-
-		for (a = 0; a < n; a++)
+		if (n <= 0)
 		{
-			for (b = 0 ; b < n; b++)
-			{
-				if (b == a)
-					_putchar('\\');
-				else if (b < a)
-					_putchar(' ');
-			}
 			_putchar('\n');
+		} else
+		{
+
+
+			int a, b;
+
+			for (a = 0; a < n; a++)
+			{
+				for (b = 0 ; b < n; b++)
+				{
+					if (b == a)
+						_putchar('\\');
+					else if (b < a)
+						_putchar(' ');
+				}
+				_putchar('\n');
+			}
 		}
 }
