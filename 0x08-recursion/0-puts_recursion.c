ff2fe13e8757207a4b0156c6bac433ec230d179c
codeained <jothamokoth@gmail.com>
2023-09-27 14:00:53 +0000

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
-#include "_putchar.c"
 
 /**
- * main - function that prints a string, followed by a new line.
+ * _puts_recursion - Prints a string to the console using recursion.
+ *
  * @s : the string characters to be printed
+ *
  * Return: Always 0.
  */
 
@@ -12,15 +13,10 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
-			return;
 	}
-	 _puts_recursion(s + 1);
-	 _putchar(*s);
-}
-int main(void)
-{
-	char s[] = "Puts with recursion";
-
-	_puts_recursion(s);
-	return (0);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
